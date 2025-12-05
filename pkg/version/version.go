f48f8fff47f70296c57ab40c28313b61559cb1b4
cgil <lao.tseu.is.alive@gmail.com>
2025-12-05 18:13:03 +0100

    added purego

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "goSwarmSimulation"
 	AppSnake   = "go-swarm-simulation"
-	VERSION    = "0.1.0"
+	VERSION    = "0.1.1"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/go-swarm-simulation"
 	REVISION   = "unknown"
 	BuildStamp = "unknown"
