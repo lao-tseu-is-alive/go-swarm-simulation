64c0a16e5769173de30ddfa1d0d349074da3554c
cgil <lao.tseu.is.alive@gmail.com>
2025-12-05 18:25:45 +0100

    removed arm

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "goSwarmSimulation"
 	AppSnake   = "go-swarm-simulation"
-	VERSION    = "0.1.2"
+	VERSION    = "0.1.3"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/go-swarm-simulation"
 	REVISION   = "unknown"
 	BuildStamp = "unknown"
