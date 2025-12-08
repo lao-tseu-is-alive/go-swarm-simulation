6a798085a91d0252fc68ccfc8de4316698f41a5c
cgil <lao.tseu.is.alive@gmail.com>
2025-12-08 09:50:25 +0100

    trying to build

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "goSwarmSimulation"
 	AppSnake   = "go-swarm-simulation"
-	VERSION    = "0.1.3"
+	VERSION    = "0.1.4"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/go-swarm-simulation"
 	REVISION   = "unknown"
 	BuildStamp = "unknown"
