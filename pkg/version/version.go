85c2d1de67659e8832c34ba75c045d4e025c9eac
cgil <lao.tseu.is.alive@gmail.com>
2025-12-08 10:42:24 +0100

    removed windows build for now

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "goSwarmSimulation"
 	AppSnake   = "go-swarm-simulation"
-	VERSION    = "0.1.6"
+	VERSION    = "0.1.7"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/go-swarm-simulation"
 	REVISION   = "unknown"
 	BuildStamp = "unknown"
