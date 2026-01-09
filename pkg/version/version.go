5af0dee78fb06b41fb005c2f65aae3d2db10824b
cgil <lao.tseu.is.alive@gmail.com>
2026-01-09 16:21:13 +0100

    corrected boids jittering

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "goSwarmSimulation"
 	AppSnake   = "go-swarm-simulation"
-	VERSION    = "0.2.2"
+	VERSION    = "0.2.3"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/go-swarm-simulation"
 	REVISION   = "unknown"
 	BuildStamp = "unknown"
